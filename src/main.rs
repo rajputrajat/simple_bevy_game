db2fd36d32c97e4ff2cd7ad9fbefb0ec6d2774ee
Rajat Rajput <rajputrajat@gmail.com>
2022-02-27 18:24:17 +0530

    first plugin

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
 }
 
@@ -36,5 +35,15 @@ fn add_people(mut commands: Commands) {
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in query.iter() {
         println!("hello {}", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
     }
 }
