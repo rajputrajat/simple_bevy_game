8035070d235b74d3e7825eac9e0a6d16c986f661
Rajat Rajput <rajputrajat@gmail.com>
2022-02-27 17:53:47 +0530

    greet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Person;
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("aloo gobhi".to_owned()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("paneer bhurji".to_owned()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("chokha".to_owned()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}", name.0);
+    }
 }
